896a45f9c3ae8d9016b3041b4ebc760128c2cffe
Jo Allam <joallam2008@gmail.com>
2026-07-21 23:27:07 +0300

    Fix Lighthouse CI server

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,15 +1,36 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: "./frontend/build"
+      startServerCommand: "npx serve -s frontend/build -l 3000",
+      url: [
+        "http://localhost:3000"
+      ],
+      numberOfRuns: 1
     },
+
     assert: {
       assertions: {
-        "categories:performance": ["warn", { minScore: 0.5 }],
-        "categories:accessibility": ["warn", { minScore: 0.5 }],
-        "categories:best-practices": ["warn", { minScore: 0.5 }]
+        "categories:performance": [
+          "warn",
+          {
+            minScore: 0.5
+          }
+        ],
+        "categories:accessibility": [
+          "warn",
+          {
+            minScore: 0.5
+          }
+        ],
+        "categories:best-practices": [
+          "warn",
+          {
+            minScore: 0.5
+          }
+        ]
       }
     },
+
     upload: {
       target: "temporary-public-storage"
     }
